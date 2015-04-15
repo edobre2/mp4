0e883ba5993ad55c02420defe03b0ae464b8b0ae
albdaw <adawod2@uic.edu>
2015-04-15 15:23:32 -0500

    whatever

--- src/mp4/Client.java
+++ src/mp4/Client.java
@@ -6,7 +6,7 @@ class Client {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("test2");
+		System.out.println("test3");
 	}
 	
 	public void GetSalesFromPrevDay() {
