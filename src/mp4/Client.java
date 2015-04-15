3d654ca14747235ceaf15d56f1957f58bb75f20f
albdaw <adawod2@uic.edu>
2015-04-15 15:44:39 -0500

    client and product changes

--- src/mp4/Client.java
+++ src/mp4/Client.java
@@ -5,23 +5,37 @@ class Client {
 	private Projection projection;
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		System.out.println("test3");
+		// loop through:
+		// ask user what they want to do
+		// list out options
+		// call function based on choice
 	}
 	
 	public void GetSalesFromPrevDay() {
-		
+		// let user input burgers
+		// >> Enter # of hamburgers sold
+		// 7
+		// create an instance of HamburgerFactory
+		// createBurger(7);
+		// for every burger type
 	}
 
-	public Product[] GetShoppingList() {
-		return null;
+	public void GetShoppingList() {
+		// call generate shopping list
+		// print result
+	
 	}
 	
 	public void ListInventory() {
-		
+		// call inventory.ListInventory()
 	}
 	
 	public void AddToInventory() {
-		
+		// ask user what they want to add
+		// ask for expiration date
+		// ask for amount
+		// make new product and call inventory.AddProduct()
 	}
 }
