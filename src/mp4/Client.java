80bf5c796dd480a4a108b5e9cf8781fca7357261
edobre2 <edobre2@uic.edu>
2015-04-15 00:35:40 -0500

    test2
    Signed-off-by: edobre2 <edobre2@uic.edu>

--- src/mp4/Client.java
+++ src/mp4/Client.java
@@ -7,7 +7,7 @@ class Client {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("test");
+		System.out.println("test2");
 	}
 	
 	public void GetSalesFromPrevDay() {
