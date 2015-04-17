17ca88d837e8ffccdc6f0b28dad5901a222bb229
Emanuil Dobrev <edobre2@uic.edu>
2015-04-17 10:25:25 -0500

    Condiment update

--- src/mp4/CondimentBeefPatty.java
+++ src/mp4/CondimentBeefPatty.java
@@ -2,13 +2,30 @@ package mp4;
 
 class CondimentBeefPatty extends Condiment {
 
-	public CondimentBeefPatty(IObserver[] observerList) {
+	//if provided an observer list at intialization
+	public CondimentBeefPatty(IObserver[] oList) {
+		
+		observerList = oList;
+		name = "Beef Patty";
+		
+	}
+	
+	
+	//if provided the observers at intialization
+	public CondimentBeefPatty(BurgerStoreInventory bs, Projection p) {
+		
+		observerList[0] = bs;
+		observerList[1] = p;
+		name = "Beef Patty";
 		
 	}
 
-	public void NotifyObserver() {
-		// TODO Auto-generated method stub
 
+	//calls update in BurgerStoreInventory and Projection
+	public void NotifyObserver() {
+		
+		observerList[0].update(this);
+		observerList[1].update(this);
 	}
 
 }
--- src/mp4/CondimentBun.java
+++ src/mp4/CondimentBun.java
@@ -2,14 +2,30 @@ package mp4;
 
 class CondimentBun extends Condiment {
 
-	public CondimentBun(IObserver[] observerList) {
+	//if provided an observer list at intialization
+	public CondimentBun(IObserver[] oList) {
+		
+		observerList = oList;
+		name = "Bun";
+		
+	}
+	
+	
+	//if provided the observers at intialization
+	public CondimentBun(BurgerStoreInventory bs, Projection p) {
+		
+		observerList[0] = bs;
+		observerList[1] = p;
+		name = "Bun";
 		
 	}
 
-	@Override
+	
+	//calls update in BurgerStoreInventory and Projection
 	public void NotifyObserver() {
-		// TODO Auto-generated method stub
-
+		
+		observerList[0].update(this);
+		observerList[1].update(this);
 	}
 
 }
