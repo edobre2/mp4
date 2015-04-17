cd5a21913f174906ce6b7cc7464f5996deba851a
Jonathan Villanueva <jvilla47@uic.edu>
2015-04-17 12:11:09 -0500

    created constructor and createBurger

--- src/mp4/CheeseburgerFactory.java
+++ src/mp4/CheeseburgerFactory.java
@@ -3,12 +3,23 @@ package mp4;
 class CheeseburgerFactory extends BurgerFactory {
 
 	public CheeseburgerFactory(IObserver inventory, IObserver projection) {
-		
+		IObserver[] observerList = new IObserver[2];
+		observerList[0] = inventory;
+		observerList[1] = projection;
+		ingredientCnt = 3 ;
+		ingredientList = new Condiment[ingredientCnt];
+		//initalize the 3 condiments for the cheeseburger,
+		ingredientList[0] = new CondimentBun(observerList);
+		ingredientList[1] = new CondimentCheese(observerList);
+		ingredientList[2] = new CondimentBeefPatty(observerList);
+
 	}
 	
-	@Override
 	public void createBurger(int num) {
-		// TODO Auto-generated method stub
+		for(Condiment c : ingredientList){//for each condiment c in the ingredient list
+			c.Amount(num);//set the amount of each ingredient
+			c.NotifyObserver();
+		}
 
 	}
 
