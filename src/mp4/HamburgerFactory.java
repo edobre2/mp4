3f81599acfdd0eb74421548c1b835a81d9775134
Jonathan Villanueva <jvilla47@uic.edu>
2015-04-17 12:28:01 -0500

    updated Hamburger factory

--- src/mp4/HamburgerFactory.java
+++ src/mp4/HamburgerFactory.java
@@ -3,13 +3,21 @@ package mp4;
 class HamburgerFactory extends BurgerFactory {
 
 	public HamburgerFactory(IObserver inventory, IObserver projection) {
-		
+		IObserver[] observerList = new IObserver[2];
+		observerList[0] = inventory;
+		observerList[1] = projection;
+		ingredientCnt = 2 ;
+		ingredientList = new Condiment[ingredientCnt];
+		//initalize the 3 condiments for the cheeseburger,
+		ingredientList[0] = new CondimentBun(observerList);
+		ingredientList[1] = new CondimentBeefPatty(observerList);
 	}
 	
-	@Override
 	public void createBurger(int num) {
-		// TODO Auto-generated method stub
-
+		for(Condiment c : ingredientList){//for each condiment c in the ingredient list
+			c.Amount(num);//set the amount of each ingredient
+			c.NotifyObserver();
+		}
 	}
 
 }
